2f489262e1c71d9b32e347be4749e46b75587a7e
laufersteppenwolf <laufersteppenwolf@gmail.com>
2014-06-04 18:39:47 +0200

    Fix a double defined line
    Change-Id: I20f02c2582614e4b9abebc5b9ff9f076da8cdce1

--- p880/p880-vendor-blobs.mk
+++ p880/p880-vendor-blobs.mk
@@ -92,7 +92,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p880/proprietary/etc/firmware/nvavp_os_eff00000.bin:system/etc/firmware/nvavp_os_eff00000.bin \
     vendor/lge/p880/proprietary/etc/firmware/nvavp_vid_ucode.bin:system/etc/firmware/nvavp_vid_ucode.bin \
     vendor/lge/p880/proprietary/etc/firmware/nvavp_vid_ucode_alt.bin:system/etc/firmware/nvavp_vid_ucode_alt.bin \
-    vendor/lge/p880/proprietary/etc/firmware/BCM4330B1_002.001.003.0550.0633.hcd:system/etc/firmware/BCM4330B1_002.001.003.0550.0633.hcd \
     vendor/lge/p880/proprietary/lib/libddp_omx.so:system/lib/libddp_omx.so \
     vendor/lge/p880/proprietary/lib/libstagefright_soft_ddpdec.so:system/lib/libstagefright_soft_ddpdec.so \
     vendor/lge/p880/proprietary/lib/libSR_AudioIn.so:system/lib/libSR_AudioIn.so \
